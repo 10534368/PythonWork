e3259851effd37c60f192d426f10f1b2f9868319
10534368 <1053443368@qq.com>
2018-11-19 14:35:45 +0800

    Default
    Signed-off-by: 10534368 <1053443368@qq.com>

--- xxshuo.py
+++ xxshuo.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
+
+from selenium import webdriver
 
 a=webdriver.Chrome()
 a.get("http://127.0.0.1:8000")
