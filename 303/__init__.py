aa9b011eb883feac3fc5983b78bb905fd2d35355
10534368 <1053443368@qq.com>
2018-11-20 17:45:22 +0800

    Default
    Signed-off-by: 10534368 <1053443368@qq.com>

--- 303/__init__.py
+++ 303/__init__.py
@@ -1,1 +1,14 @@
-#-*- coding: utf-8 -*-
+nums = list(input())
+cnt = int(input())
+n, j, i = len(nums), cnt, 0
+while j > 0 and i < n - 1:
+    if nums[i] >= nums[i + 1]:
+        i += 1
+    else:
+        nums.pop(i)
+        j -= 1
+        n -= 1
+        i = i - 1 if i > 0 else 0
+if j > 0:
+    nums = nums[:-j]
+print(''.join(nums))
